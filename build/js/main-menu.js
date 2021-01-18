2a21bb50aa8a5f08a04a5918d86e54f2abc56dfd
Евгений <lazarev.haven@gmail.com>
2021-01-18 23:17:08 +0300

    сделал блок: о нас

--- build/js/main-menu.js
+++ build/js/main-menu.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const header = document.querySelector('.page-header');
-const navToggle = document.querySelector('.page-header__menu-button');
+var header = document.querySelector('.page-header');
+var navToggle = document.querySelector('.page-header__menu-button');
 
 header.classList.remove('page-header--nojs');
 
-navToggle.addEventListener('click', function() {
+navToggle.addEventListener('click', function () {
   if (header.classList.contains('page-header--close-menu')) {
     header.classList.remove('page-header--close-menu');
     header.classList.add('page-header--open-menu');
