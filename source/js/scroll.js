6bc319fc0d69df3e949734c5961c89f752ce2be4
Евгений <lazarev.haven@gmail.com>
2021-01-20 02:24:09 +0300

    добавил плавный скролл на js

---
+++ source/js/scroll.js
@@ -0,0 +1,12 @@
+var links = document.querySelectorAll('.menu__link');
+for (let link of links) {
+    link.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        const id = link.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
