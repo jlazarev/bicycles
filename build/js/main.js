ba86d3552cf076d02fbff838e7a4584cee241814
Евгений <lazarev.haven@gmail.com>
2021-01-23 03:29:15 +0300

    исправил меню и ссылки

--- build/js/main.js
+++ build/js/main.js
@@ -6,6 +6,7 @@
 
   var header = document.querySelector('.page-header');
   var navToggle = document.querySelector('.page-header__menu-button');
+  var body = document.querySelector('.page__body');
 
   header.classList.remove('page-header--nojs');
 
@@ -13,9 +14,11 @@
     if (header.classList.contains('page-header--close-menu')) {
       header.classList.remove('page-header--close-menu');
       header.classList.add('page-header--open-menu');
+      body.classList.add('page__body--no-scroll');
     } else {
       header.classList.add('page-header--close-menu');
       header.classList.remove('page-header--open-menu');
+      body.classList.remove('page__body--no-scroll');
     }
   });
 
